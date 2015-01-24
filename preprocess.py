08d51081560672f72c96ac994881afa7b52d9a89
Hans Tunggajaya <hans.tunggajaya@gmail.com>
2015-01-24 15:36:29 +0800

    v0.2 Fix logic mistakes Handle anacrusis

--- preprocess.py
+++ preprocess.py
@@ -1,60 +1,88 @@
 __author__ = 'redhat'
 
 from music21 import converter as c
+import os
 
-song = c.parse('corpus/0 Diademata.xml')
+song = c.parse('corpus/0 Christus,_Der_Ist_Mein_Leben.xml')
 
 states = []
 first = True
-
 for stream in song.parts[0]:
     if first:
         # skip the Instrument stream.
         first = False
         continue
 
-    bar = stream.notesAndRests
+    if stream.number == 1 and stream.duration != stream.barDuration:
+        # it's an anacrusis
+        print('anacrusis')
+        bar = stream.notesAndRests
+        pickups = []
 
-    current_fighead = None
-    fig_start_at_beat = None
-    fig_notes = None
+        first = True
+        for elem in bar:
+            if elem.isNote:
+                if first:
+                    fighead = elem.midi
+                    first = False
+                pickups.append(elem.midi)
+                pickups.append(elem.quarterLength)
+            elif elem.isRest:
+                pickups.append('rest')
+                pickups.append(elem.quarterLength)
+        states.append(("pickup", tuple(pickups)))
+        continue
+
+
+    bar = stream.notesAndRests
 
     for elem in bar:
         if elem.isChord:
             # assume elem is Chord
+            try:
+                prev_fig_chord = chord
+            except NameError:
+                pass
             chord = elem.figure
-            print(chord)
             is_fighead = 1
+
         elif elem.isNote:
             # elem is a note
 
             # when at new fighead
-            if is_fighead == 1 or elem.beat == 0.0:
+            if is_fighead == 1 or elem.beat == 1.0:
+                # many variables are undefined when iterating the first bar
 
-                if fig_start_at_beat:
-                    # if fig_start_at_beat is already set
-                    fig_duration = (elem.beat - fig_start_at_beat) % 4
-                else:
-                    # if not, current beat is where the figure starts
-                    fig_start_at_beat = elem.beat
+                try:
+                    # if fig_start_at_beat undefined, define it for the new
+                    # figure
+                    prev_fig_start_at_beat = fig_start_at_beat
+                    prev_fig_duration = fig_duration
+                    prev_fig_notes = fig_notes
+                    prev_fighead = current_fighead
 
-                # get figure notes and set it empty for next figure
-                prev_fig_notes = fig_notes
-                fig_notes = []
-
-                # if fighead is already defined
-                if current_fighead:
                     states.append((tuple(prev_fig_notes),
-                                   chord,
-                                   fig_duration,
-                                   fig_start_at_beat,
-                                   current_fighead))
-                current_fighead = elem.midi
-                is_fighead = 0
+                                   prev_fig_chord,
+                                   prev_fig_duration,
+                                   prev_fig_start_at_beat,
+                                   prev_fighead))
+                except NameError:
+                    pass
+
+                current_fighead = elem.midi  # set new fighead
+                fig_duration = 0
+                fig_start_at_beat = elem.beat  # determine beat of the new
+                # figure
+                fig_notes = []  #set empty for the new figure
+                is_fighead = 0  # next note may not be a fighead
+
+            #append notes to fig_notes
             fig_notes.append(elem.midi)
+            fig_notes.append(elem.quarterLength)
+            fig_duration += elem.quarterLength
         elif elem.isRest:
             # elem is a rest
-            print('rest')
+            states.append(('rest', elem.quarterLength, elem.beat))
 
 for state in states:
     print(state)
