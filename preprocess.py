50eb21248c42b1af526e9b0dd96a56043ce0cbf0
Hans Tunggajaya <hans.tunggajaya@gmail.com>
2015-01-29 18:09:48 +0800

    Add several new methods. Made dictionary that translates from states and actions to int and vice versa.

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ __author__ = 'redhat'
 
 from music21 import converter, note, harmony, stream
 import os
-import pprint
+from pprint import pprint
 
 # Method name should start with a verb
 
@@ -102,72 +102,71 @@ def get_corpus(corpus_dir):
             filenames.append(corpus_dir + f)
     return filenames
 
-def make_dict_of_elem(list_of_tuples):
-    """
-    Assume that we have a list of tuples.
-    An element in n-th coordinate of a tuple comes from a set.
-    The set contains all elements that are in n-th coordinate of all
-    tuples in the list.
-    Map all the elements from the set to a nonnegative integer and vice versa.
-    Returns 3 lists.
-    The first list contains dictionaries of k-th element to nonnegative integer
-    The second is the other way around.
-    the third contains size of each set
-    """
+# def make_dict_of_elem(list_of_tuples):
+#     """
+#     Assume that we have a list of tuples.
+#     An element in n-th coordinate of a tuple comes from a set.
+#     The set contains all elements that are in n-th coordinate of all
+#     tuples in the list.
+#     Map all the elements from the set to a nonnegative integer and vice versa.
+#     Returns 3 lists.
+#     The first list contains dictionaries of k-th element to nonnegative integer
+#     The second is the other way around.
+#     the third contains size of each set
+#     """
+#
+#     # initialize list of dictionaries
+#     list_to_int = []
+#     list_to_elem = []
+#     list_size = []
+#
+#     # assume that all tuples have same length
+#     for i in range(len(list_of_tuples[0])):
+#         list_to_int.append({})  # key: elem, value: int
+#         list_to_elem.append({})  # key: int, value: elem
+#         counter = 0
+#         for tuple in list_of_tuples:
+#             # unpack elements in the tuple
+#             # store it in dictionary if the key doesn't exist, pass otherwise
+#             if tuple[i] not in list_to_int[i]:
+#                 list_to_int[i][tuple[i]] = counter
+#                 list_to_elem[i][counter] = tuple[i]
+#                 counter += 1
+#         list_size.append(counter)
+#     return (list_to_int, list_to_elem, list_size)
+#
+# def convert_elem_of_states_to_int(list_of_lists_of_tuples):
+#     """
+#     returns states that are tuple of nonnegative integers, e.g.
+#     (1,1,1,1,2) instead of ((0, 1.0), 'C', 1.0, 1.0, 0)
+#     """
+#
+#     # flatten list of list of tuples to list of tuples
+#     flatten_states = [item for list_of_tuples in
+#                       list_of_lists_of_tuples for item in list_of_tuples]
+#
+#     global dict_elem_to_int, dict_int_to_elem, elem_set_sizes
+#     dict_elem_to_int, dict_int_to_elem, elem_set_sizes = \
+#         make_dict_of_elem(flatten_states)
+#
+#     new_all_states = []
+#     for states in list_of_lists_of_tuples:
+#         new_states = []
+#         for state in states:
+#             new_state = []
+#             for i in range(len(state)):
+#                 new_elem = dict_elem_to_int[i][state[i]]
+#                 new_state.append(new_elem)
+#             new_states.append(tuple(new_state))
+#         new_all_states.append(new_states)
+#     return new_all_states
 
-    # initialize list of dictionaries
-    list_to_int = []
-    list_to_elem = []
-    list_size = []
-
-    # assume that all tuples have same length
-    for i in range(len(list_of_tuples[0])):
-        list_to_int.append({})  # key: elem, value: int
-        list_to_elem.append({})  # key: int, value: elem
-        counter = 0
-        for tuple in list_of_tuples:
-            # unpack elements in the tuple
-            # store it in dictionary if the key doesn't exist, pass otherwise
-            if tuple[i] not in list_to_int[i]:
-                list_to_int[i][tuple[i]] = counter
-                list_to_elem[i][counter] = tuple[i]
-                counter += 1
-        list_size.append(counter)
-    return (list_to_int, list_to_elem, list_size)
-
-def convert_elem_of_states_to_int(list_of_lists_of_tuples):
-    """
-    returns states that are tuple of nonnegative integers, e.g.
-    (1,1,1,1,2) instead of ((0, 1.0), 'C', 1.0, 1.0, 0)
-    """
-
-    # flatten list of list of tuples to list of tuples
-    flatten_states = [item for list_of_tuples in
-                      list_of_lists_of_tuples for item in list_of_tuples]
-
-    global dict_elem_to_int, dict_int_to_elem, elem_set_sizes
-    dict_elem_to_int, dict_int_to_elem, elem_set_sizes = \
-        make_dict_of_elem(flatten_states)
-
-    new_all_states = []
-    for states in list_of_lists_of_tuples:
-        new_states = []
-        for state in states:
-            new_state = []
-            for i in range(len(state)):
-                new_elem = dict_elem_to_int[i][state[i]]
-                new_state.append(new_elem)
-            new_states.append(tuple(new_state))
-        new_all_states.append(new_states)
-    return new_all_states
-
-def convert_states_with_int_elem_to_int(list_of_lists_of_tuples, elem_set_sizes):
-    pass
-
+def make_flat_list(list_of_lists):
+    flat_list = [item for lists in list_of_lists for item in lists]
+    return flat_list
 
 def make_action_set(trajectories):
-    all_states = [state for trajectory in
-                      trajectories for state in trajectory]
+    all_states = make_flat_list(trajectories)
     action_set_by_duration = {}
     for state in all_states:
         if state[1] != 'pickup':
@@ -194,26 +193,101 @@ def get_start_states(trajectories):
     return set(start_states)
 
 
+def map_tuples_to_int(states):
+    """
+    return a 2-tuple of dict of item to int and int to item
+    """
+    tuple_to_int_dict = {}
+    int_to_tuples_dict = {}
+    counter = 0
+    states = make_flat_list(states)
+    for state in states:
+        if state not in tuple_to_int_dict:
+            tuple_to_int_dict[state] = counter
+            int_to_tuples_dict[counter] = state
+            counter += 1
+    return tuple_to_int_dict, int_to_tuples_dict
+
+def map_item_inside_list_of_list(list_of_lists, item_mapper):
+    # item_mapper is a dict
+    mapped_list_of_list = []
+    for lists in list_of_lists:
+        mapped_item_list = []
+        for item in lists:
+            int_state = item_mapper[item]
+            mapped_item_list.append(int_state)
+        mapped_list_of_list.append(mapped_item_list)
+    return mapped_list_of_list
+
+def compute_trajectory(int_s, int_a, states_dict, actions_dict):
+    # states_dict: (states_to_int, int_to_states)
+    # actions_dict: (actions_to_int, int_to_actions)
+    # find a that makes s transition to s_prime
+
+    s = states_dict[1][int_s]
+    a = actions_dict[1][int_a]
+    s_prime = (a[0], a[1], sum(a[0][1::2]), s[2]+s[3], a[0][0])
+    return states_dict[0][s_prime]
+
+
+
+def compute_next_state(int_s, int_a, states_dict, actions_dict):
+    # states_dict: (states_to_int, int_to_states)
+    # actions_dict: (actions_to_int, int_to_actions)
+    # a = ((next_fig_seq_of_notes), next_chord_name)
+    # s = ((fig_seq_of_notes), chord_name, duration, beat, fighead_note)
+    # s_prime = ((next_fig_seq_of_notes),
+    # next_chord_name,
+    # duration = sum of duration of each note in next_fig_seq_of_notes,
+    # beat = duration of s + beat s in,
+    # fighead_note = the first note of next_fig_seq_of_notes)
+
+    s = states_dict[1][int_s]
+    a = actions_dict[1][int_a]
+    s_prime = (a[0], a[1], sum(a[0][1::2]), s[2]+s[3], a[0][0])
+    return states_dict[0][s_prime]
+
 def generate_features(state, action):
     # need numpy array
     pass
 
-def compute_next_state(int_s, int_a):
-    s = states_dict[int_s]
-    a = actions_dict[int_a]
-    s_prime = (a[0], a[1], sum(a[0][::2]), s[2]+s[3], a[0][0])
-    return s_prime
-
-
 if __name__ == "__main__":
     filenames = get_corpus('corpus/')
-    all_states = []
+    list_of_states_per_song = []
     for filename in filenames:
         states = parse(filename)
-        all_states.append(states)
-    pprint.pprint(all_states)
-    pprint.pprint(make_action_set(all_states))
-    pprint.pprint(get_start_states(all_states))
+        list_of_states_per_song.append(states)
+
+    # TRAJECTORIES is list of trajectory
+    # Trajectory is (s1, a1, s2, a2, s3, a3, ...)
+
+
+
+    actions_set = make_action_set(list_of_states_per_song)
+    start_states = get_start_states(list_of_states_per_song)
+    terminal_states = get_terminal_states(list_of_states_per_song)
+    state_int_dict, int_state_dict = map_tuples_to_int(list_of_states_per_song)
+    new_list_of_states_per_song = map_item_inside_list_of_list(list_of_states_per_song,
+                                                   state_int_dict)
+
+    print("\nACTIONS SET")
+    pprint(actions_set)
+
+    print("\nSTART_STATES")
+    pprint(start_states)
+
+    print("\nTERMINAL_STATES")
+    pprint(terminal_states)
+
+    print("\nSTATE to INT DICT")
+    pprint(state_int_dict)
+
+    print("\nINT to STATE DICT")
+    pprint(int_state_dict)
+
+    print("\nNEW LIST OF STATES PER SONG")
+    pprint(new_list_of_states_per_song)
+
     # states_with_int_elem = convert_elem_of_states_to_int(all_states)
     # states_of_int = convert_states_with_int_elem_to_int(states_with_int_elem,
     #                                                     elem_set_sizes)
