509ad2511482dd5df603f57f6974c47382f3652e
Hans Tunggajaya <hans.tunggajaya@gmail.com>
2015-01-30 21:17:14 +0800

    generate_features is working with minor bugs:
    beat is float, most common x and a half. Current solution is to multiply by 2.
    
    Current way of parse chord is very simple. Take the first character of chord name string.

--- generate_features.py
+++ generate_features.py
@@ -3,6 +3,8 @@ __author__ = 'redhat'
 from scipy import sparse
 import numpy as np
 from constants import *
+from preprocess import load_obj
+from pprint import pprint
 
 def tuple_to_int(tup, elem_size):
     array = np.array(tup)
@@ -30,13 +32,14 @@ def map_tup_to_bin_array(tup, min_elem, max_elem):
     Big-Endian
     """
     coord_size = np.array(max_elem) - np.array(min_elem) + 1
-    bin_array = np.array([0] * sum(coord_size))
+    bin_array_length = sum(coord_size)
     coord_size = np.concatenate((np.array([0]), coord_size))
-    pos = 0
+    pos = []
+    index = 0
     for i in range(len(tup)):
-        pos = pos + coord_size[i]
-        bin_array[pos + tup[i] - min_elem[i]] = 1
-    return bin_array
+        index = index + coord_size[i]
+        pos.append(index + tup[i] - min_elem[i])
+    return np.array(pos), bin_array_length
 
 def compute_next_state(state, action):
     # states_dict: (states_to_int, int_to_states)
@@ -63,7 +66,7 @@ def get_features_range(states_dict, actions_dict, terminal_states):
             if not is_valid_action(state, action):
                 continue
 
-            features_vector = compute_features(state, action)
+            features_vector = compute_features(state, action, terminal_states)
 
             try:
                 min_features_vector = np.minimum(min_features_vector, features_vector)
@@ -74,37 +77,16 @@ def get_features_range(states_dict, actions_dict, terminal_states):
 
     return (min_features_vector, max_features_vector)
 
-
-def generate_features(states_dict, actions_dict, terminal_states):
-    state_action_sizes = (len(states_dict[0]), len(actions_dict[0]))  #(# states, # actions)
-    min_feat, max_feat = get_features_range(states_dict, actions_dict,
-                                     terminal_states)
-
-    # PROBLEM, NEED TO create row, col, value lists before making sparse
-    # matrix.
-
-    for state in states_dict[0]:
-        if state in terminal_states:
-            continue
-        for action in actions_dict:
-            if not is_valid_action(state, action):
-                continue
-            int_s = states_dict[0][state]
-            int_a = actions_dict[0][action]
-            features_vector = compute_features(state, action)
-
-            row = tuple_to_int((int_s, int_a), state_action_sizes)
-            col = map_tup_to_bin_array(features_vector, min_feat, max_feat)
-            value = np.ones(len(col))
-
-    return sparse.coo_matrix((value, (row, col)))
-
-
 # These methods are assumed to have input the original state and action not
 # integers.
+
+def parse_chord(chord):
+    pass
+
 def compute_root_movement(state, action):
-    current_chord = CHORD_ROOT_TO_INT[state[1]]
-    next_chord = CHORD_ROOT_TO_INT[action[1]]
+    # TROUBLE, how to parse chord?
+    current_chord = CHORD_ROOT_TO_INT[state[1][0]]
+    next_chord = CHORD_ROOT_TO_INT[action[1][0]]
     return next_chord - current_chord
 
 def compute_figure_head_movement(state, action):
@@ -112,7 +94,7 @@ def compute_figure_head_movement(state, action):
 
 def get_fig_contour(state):
     "UP or DOWN or STAY"
-    fig_contour = state[-2] - state[0]
+    fig_contour = state[0][-2] - state[0][0]
     if fig_contour > 0:
         return 1
     elif fig_contour < 0 :
@@ -121,9 +103,9 @@ def get_fig_contour(state):
         return 0
 
 def compute_next_fig_beat(state):
-    return state[2] + state[3]
+    return 2*(state[2] + state[3])
 
-def is_to_term_state(action, TERM_STATES):
+def is_to_term_state(state, action, TERM_STATES):
     if compute_next_state(state, action) in TERM_STATES:
         return 1
     return 0
@@ -138,16 +120,61 @@ def is_to_rest(action):
         return 1
     return 0
 
-def compute_features(state, action):
-    tup = (
-        compute_root_movement(state, action),
-        compute_figure_head_movement(state, action),
-        get_fig_contour(state),
-        compute_next_fig_beat(state),
-        is_to_term_state(state),
-        is_to_rest(action),
-    )
+def compute_features(state, action, term_states):
+    if state[1] == 'rest' or action[1] == 'rest':
+        tup = (0, 0, 0, 0, 0, 1)
+    else:
+        tup = (
+            compute_root_movement(state, action),
+            compute_figure_head_movement(state, action),
+            get_fig_contour(state),
+            compute_next_fig_beat(state),
+            is_to_term_state(state, action, term_states),
+            is_to_rest(action),
+        )
     return np.array(tup)
 
+############
+
+def generate_features(states_dict, actions_dict, terminal_states):
+    state_action_sizes = (len(states_dict[0]), len(actions_dict[0]))  #(# states, # actions)
+    min_feat, max_feat = get_features_range(states_dict, actions_dict,
+                                     terminal_states)
+    num_of_rows = state_action_sizes[0] * state_action_sizes[1]
+
+    # Use DOK sparse matrix
+
+    first = True
+    for state in states_dict[0]:
+        if state in terminal_states:
+            continue
+        for action in actions_dict[0]:
+            if not is_valid_action(state, action):
+                continue
+            int_s = states_dict[0][state]
+            int_a = actions_dict[0][action]
+            features_vector = compute_features(state, action, terminal_states)
+
+            row = tuple_to_int((int_s, int_a), state_action_sizes)
+            if first:
+                # when first iteration, initialize sparse matrix after
+                # having computer number of columns of features vector
+                col, num_of_cols = map_tup_to_bin_array(features_vector,
+                                                        min_feat,
+                                                        max_feat)
+                sparse_feature_matrix = sparse.dok_matrix((num_of_rows,
+                                                           num_of_cols),
+                                                          dtype=np.uint8)
+                first = False
+            else:
+                col, _ = map_tup_to_bin_array(features_vector, min_feat, max_feat)
+            for j in col:
+                sparse_feature_matrix[row, j] = 1
+
+    return sparse_feature_matrix
+
 if __name__ == "__main__":
-    pass
+    STATES_DICT = load_obj('STATES_DICT')
+    ACTIONS_DICT = load_obj('ACTIONS_DICT')
+    TERM_STATES = load_obj('TERM_STATES')
+    pprint(generate_features(STATES_DICT, ACTIONS_DICT, TERM_STATES))
